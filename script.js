2bbf3918413832e95f2d2dfcbcff8bbd4d4d13cc
ankit-yadav-17 <119732393+ankit-yadav-17@users.noreply.github.com>
2023-06-16 09:25:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 //complete this code
 class Animal {
 	constructor(species){
-		this._species = species
+		this.species = species
 	}
 	get species(){
-		return this._species
+		return this.species
 	}
 	makeSound(){
-		console.log("The animal makes a sound")
+		console.log("The "+this.species+" makes a sound")
 	}
 }
 
